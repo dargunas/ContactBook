34cb632e199c1bb46754756c48b27ff005f6be49
dargunas <dargunas@gmail.com>
2021-12-21 20:52:30 +0200

    commit before doing something stupid

---
+++ src/main/java/contacts/EditContactsInJsonFile.java
@@ -0,0 +1,12 @@
+package contacts;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.FileReader;
+
+public class EditContactsInJsonFile {
+
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    FileReader
+}
